4f415d65b08bac6197b7e4cb7c444bcb2017a970
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-03-09 13:53:58 -0600

    ff7 update

---
+++ FF-LeagueEdition/FF-LeagueEdition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF_LeagueEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fsm/fsm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fsm
+{
+    class Program
+    {
+
+        public void leggo()
+        {
+
+        }
+        private List<Enum> States;
+
+        bool AddState(Enum state)
+        {
+            if (state.GetType() == typeof(Enum))
+            {
+                States.Add(state);
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}

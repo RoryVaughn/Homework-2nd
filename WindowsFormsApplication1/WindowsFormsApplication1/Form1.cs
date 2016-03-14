2129e2b7f12d31b1684a14231f784c2db68a8363
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-03-14 17:59:39 -0500

    big list

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -18,12 +18,17 @@ namespace WindowsFormsApplication1
         }
 
 
+
         public void attack_Click(object sender, EventArgs e)
         {
-            List<int> theData = new List<int> { 5, 6, 2, 1, 6, 4, 3, 6, 2, 3, 6, 1, 6, 3, 3, 4, 1 };
+            List<int> theData = new List<int> { 5, 6, 2, 1, 6, 4, 6,1,2,3,5,4,3,2,2,1,2,2,1,4,4,5,4,3,3,5,5,6, 3, 6, 2, 3, 6, 1, 6, 3, 3, 4, 1 };
+            //This loop adds all of the integers to a list and stops adding when every int has been added.
             for (int i = 0; i < theData.Count; ++i)
             {
+                //this sorts the previosly declared list
                 theData.Sort();
+                //this adds the integers to the lists. It is placed behind the sort because otherwise -->
+                //the first int would not be sorteded becaus eof the loop.
                 chart1.Series["Damage"].Points.Add(theData[i]);
                 
             }

56acf91e99806ca31c4ba2db4649e31ed0998b0c
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-03-21 16:50:58 -0500

    win form

---
+++ ADGP125/ADGP125/ADGP125/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGP125
+{
+    class Team
+    {
+        interface I_Abilites
+        {
+            void M1();
+            int M1cd();
+            
+            
+        }
+        class Fighter : I_Abilites
+        {
+            public string name;
+            public int health = 500;
+            public int mana = 100;
+            public int exp = 0;
+            public int lvl = 1;
+
+            public void M1()
+            {
+                throw new NotImplementedException();
+            }
+
+           
+            public int M1cd()
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+        
+     }
+
+}
--- FF-LeagueEdition/FF-LeagueEdition/Program.cs
+++ FF-LeagueEdition/FF-LeagueEdition/Program.cs
@@ -13,6 +13,7 @@ namespace FF_LeagueEdition
 
 
 
+
             Console.ReadLine();
         }
     }

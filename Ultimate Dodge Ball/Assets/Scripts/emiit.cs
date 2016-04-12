34f06d12f42a05eb17a987a9be7f8b49ab1bc81b
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-04-12 14:02:55 -0500

    Ultimate dodgeball

---
+++ Ultimate Dodge Ball/Assets/Scripts/emiit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class emiit : MonoBehaviour {
+    public int speed = 30;
+    public GameObject Sphere;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+            {
+
+                Instantiate(Sphere, new Vector3(1 *Time.deltaTime * speed,0,16), Quaternion.identity);
+            }
+
+	}
+}
